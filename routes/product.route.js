bcf78c40147c7302bdffbc40a7b85ab434383b3f
Abinaya <abinaya282004@gmail.com>
2024-11-16 23:04:23 +0530

    route change

--- routes/product.route.js
+++ routes/product.route.js
@@ -48,7 +48,7 @@ router.post("/", verifyToken, isAdmin, async (req, res) => {
 });
 
 
-router.get("/api/products", async (req, res) => {
+router.get("/", async (req, res) => {
   try {
     const products = await Product.find(); 
     res.status(200).json({ products });
